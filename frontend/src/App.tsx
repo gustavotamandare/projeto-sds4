74ab614e38166997cce967971fb490d8cfddb4a0
gustavotamandare <gugatamandare@gmail.com>
2021-09-12 05:52:29 -0300

    Taxa de Sucesso

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Sucesso de Vendas</h5>
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
